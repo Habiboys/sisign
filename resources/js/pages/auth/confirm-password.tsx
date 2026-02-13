cfeda488af705cb2488de0808b252875f800eebb
Habiboys <nouvalhabibie18@gmail.com>
2026-02-13 14:15:04 +0700

    oke

--- resources/js/pages/auth/confirm-password.tsx
+++ resources/js/pages/auth/confirm-password.tsx
@@ -3,9 +3,9 @@ import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
 import AuthLayout from '@/layouts/auth-layout';
-import { store } from '@/routes/password/confirm/index';
 import { Form, Head } from '@inertiajs/react';
 import { LoaderCircle } from 'lucide-react';
+import { store } from '../../routes/password/confirm';
 
 export default function ConfirmPassword() {
     return (
