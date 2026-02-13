8c7b63b0833ae32f71c2dd3cc09d4f08fad6bf3c
Habiboys <nouvalhabibie18@gmail.com>
2026-02-13 14:00:37 +0700

    done

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,8 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
-use Illuminate\Support\Facades\Vite;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -21,19 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        // Force HTTPS in production
-        if (app()->environment('production')) {
-            URL::forceScheme('https');
-
-            // Force HTTPS for asset URLs
-            if (config('app.asset_url')) {
-                config(['app.asset_url' => str_replace('http://', 'https://', config('app.asset_url'))]);
-            }
-
-            // Override Vite asset URL generation to use HTTPS
-            Vite::useAssetUrl(function ($path) {
-                return secure_url($path);
-            });
-        }
+        //
     }
 }
