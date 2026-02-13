307d9314bb6ee14968e6c875c22b9446aadbd41a
Habiboys <nouvalhabibie18@gmail.com>
2026-02-13 19:51:19 +0700

    force https

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
-use App\Http\Middleware\ForceHttps;
+
+// use App\Http\Middleware\ForceHttps;
 use App\Http\Middleware\HandleAppearance;
 use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Application;
@@ -18,9 +19,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->trustProxies(at: '*');
         $middleware->encryptCookies(except: ['appearance', 'sidebar_state']);
 
-        $middleware->web(prepend: [
-            ForceHttps::class,
-        ]);
+        // $middleware->web(prepend: [
+        //     ForceHttps::class,
+        // ]);
 
         $middleware->web(append: [
             HandleAppearance::class,
